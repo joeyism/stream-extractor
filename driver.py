8eff35530c0651942be0f4d8db0e169685869f6b
joeyism <sham.joey@gmail.com>
2019-02-02 11:44:50 -0500

    kill chrome at the end to prevent many chromes killing memory

--- driver.py
+++ driver.py
@@ -1,5 +1,22 @@
+from http import client as httplib
+import socket
+
+from selenium.webdriver.remote.command import Command
 from selenium import webdriver
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 driver = webdriver.Chrome("./chromedriver", options=options)
+
+def is_alive(driver):
+    try:
+        driver.execute(Command.STATUS)
+        return True
+    except (socket.error, httplib.CannotSendRequest):
+        return False
+
+def quitdriver():
+    driver.quit()
+
+import atexit
+atexit.register(quitdriver)
--- extract.py
+++ extract.py
@@ -3,13 +3,16 @@ import os
 
 import jsbeautifier
 
-from driver import driver
+from driver import driver, is_alive
 from prompt import get_shows, prompt_for_shows
 
 def get_js(url):
     driver.get(url)
     js = [script.get_attribute("innerHTML") for script in driver.find_elements_by_tag_name("script") if "eval" in script.get_attribute("innerHTML")][0]
     beautified_js = jsbeautifier.beautify(js)
+    if is_alive(driver):
+        driver.quit()
+
     return beautified_js
 
 def extract_m3u8(url):
